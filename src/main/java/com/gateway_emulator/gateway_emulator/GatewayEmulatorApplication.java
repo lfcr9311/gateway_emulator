9ab5aef11bdd40e51f3dbf40b2ce3309360e07b6
Luis Felipe Rossi <lfcr93@gmail.com>
2025-09-21 15:42:19 -0300

    initial commit

---
+++ src/main/java/com/gateway_emulator/gateway_emulator/GatewayEmulatorApplication.java
@@ -0,0 +1,13 @@
+package com.gateway_emulator.gateway_emulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayEmulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayEmulatorApplication.class, args);
+	}
+
+}

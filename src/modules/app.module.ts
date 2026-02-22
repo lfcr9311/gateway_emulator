86b77fc6d1edf90b93e8e5c86a248117c1ec5903
Luis Felipe Rossi <lfcr93@gmail.com>
2026-02-22 01:34:57 -0300

    ajeitando repo

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from 'src/application/app.service';
+import { AppController } from 'src/infra/app.controller';
 
 @Module({
   imports: [],
